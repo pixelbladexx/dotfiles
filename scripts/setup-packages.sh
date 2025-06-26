e14d75fc6094ef27ab1510374350c1e6f3885be2
pixelbladexx <adityachand@gmail.com>
2025-06-26 15:52:21 +0530

    setup packages

---
+++ scripts/setup-packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo pacman -S --needed \
+zsh \
+starship \
+git \
+curl \
+wget \
+neovim \
+kitty \
+bat \
+fd \
+ripgrep \
+fzf \
+btop \
+polybar \
+rofi \
+cava \
+thunar \
+exo \
+pipewire pipewire-pulse \
+acpi \
+firefox \
+kvantum-qt5 \
+betterlockscreen
+#yay -S oh-my-zsh-git obsidian visual-studio-code-bin
+
